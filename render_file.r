3d5f420140a7d961146e9b06c8db571a1f69fb37
Ryan Craft <rjcraft@ncsu.edu>
2023-10-07 16:58:56 -0500

    Render

--- render_file.r
+++ render_file.r
@@ -1,4 +1,4 @@
-rmarkdown::render("D:/Fall2023/ST558/ST558Proj2/README.md", 
+rmarkdown::render("D:/Fall2023/ST558/ST558Proj2/RC ST558 Project 2.rmd", 
                   output_format = github_document,
                   
       )
